49fac709e1e721b4836578b2ffa96235271beeaf
Andrew M. C. Dawes <dawes@pacificu.edu>
2013-02-16 12:02:22 -0800

    fixed unicode issue in papers.html

--- makemain.py
+++ makemain.py
@@ -1,7 +1,8 @@
 from mako.template import Template
+import codecs
 
-titles = ['Home','Projects','Bio','Contact','Schedule','Useful Stuff']
-files = ['index','projects','bio','contact','schedule','useful']
+titles = ['Home','Projects','Papers','Bio','Contact','Schedule','Useful Stuff']
+files = ['index','projects','papers','bio','contact','schedule','useful']
 
 navtemplate = Template(filename="navlist.html")
 
@@ -18,12 +19,12 @@ for thispage,thisfile in zip(titles,files):
 
 		navlist += navtemplate.render(active=activecss, filename=filename, title=title)
 
-	maincontent = open(thisfile + '.src')
+	maincontent = codecs.open(thisfile + '.src','r','utf-8')
 	content = maincontent.read()
 
 	output = maintemplate.render(navlist=navlist, content=content)
 
-	outfile = open(thisfile + '.html','w')
+	outfile = codecs.open(thisfile + '.html','w','utf-8')
 	outfile.write(output)
 	outfile.close()
 	print("wrote " + thispage)
